f30fefc59a9b051bb3656b43043d20763fb28ecd
Igiraneza1 <134881953+Igiraneza1@users.noreply.github.com>
2025-05-04 10:46:49 +0300

    first commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,21 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cart from './Cart';
-
-import belt from '../components/images/belt.jpg';
-import gloves from '../components/images/gloves.jpg';
-import hat from '../components/images/hat.jpg';
-import dress from '../components/images/dress.jpg';
-import jacket from '../components/images/jacket.jpg';
-import jeans from '../components/images/jeans.webp';
-import pajamas from '../components/images/pajamas.jpg';
-import scarf from '../components/images/scarf.jpg';
-import short from '../components/images/short.jpg';
-import sneakers from '../components/images/sneakers.jpg';
-import swimsuit from '../components/images/swimsuit.jpg';
-import sweater from '../components/images/sweater.jpg';
-import shirt from '../components/images/shirt.jpg';
-import tshirt from '../components/images/tshirt.jpg';
-import sandal from '../components/images/sandal.jpg';
+import productsData from '../data/data.json';
 
 
 function ProductList() {
@@ -29,24 +14,7 @@ function ProductList() {
     localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
 
-  const [products] = useState([
-   
-    { id: 1, name: "Hat", price: 10, image: hat },
-    { id: 2, name: "Jeans", price: 35, image: jeans },
-    { id: 3, name: "Sneakers", price: 75, image: sneakers },
-    { id: 1, name: "Scarf", price: 10, image: scarf },
-    { id: 5,name: "Belt", price: 5, image: belt },
-    { id: 6, name: "Jacket", price: 40, image: jacket },
-    { id: 7, name: "Shorts", price: 14, image: short },
-    { id: 8, name: "Sandal", price: 30, image: sandal },
-    { id: 9, name: "Shirt", price: 5, image: shirt },
-    { id: 10, name: "Gloves", price: 20, image: gloves },
-    { id: 11, name: "Sweater", price: 30, image: sweater },
-    { id: 12, name: "Pajamas", price: 20, image: pajamas },
-    { id: 13, name: "Swimsuit", price: 40, image: swimsuit },
-    { id: 14, name: "T-shirt", price: 15, image: tshirt },
-    { id: 15, name: "Dress", price: 10, image: dress },
-  ]);
+  const[products] = useState(productsData);
 
   const addToCart = (product) => {
     const exists = cart.find(item => item.id === product.id);
@@ -70,7 +38,7 @@ function ProductList() {
               <img
                 src={product.image}
                 alt={product.name}
-                className="product-image rounded-md shadow-md shadow-black hover:border-2 hover:border-orange-600"
+                className="product-image rounded-md shadow-md shadow-black hover:border-2 hover:border-orange-600 "
               />
               <figcaption>
                 <h2 className='font-bold text-lg'>{product.name}</h2>
@@ -81,7 +49,7 @@ function ProductList() {
                   addToCart(product);
                   console.log(`${product.name} added to cart!`);
                }}
-                className=" text-black w-32 p-2 border-1 border-black rounded-full hover:bg-orange-700">
+                className=" text-black w-32 p-2 border-1 border-black rounded-full hover:bg-orange-700 focus:ring-4 focus:ring-orange-500">
                 Add to Cart
                 </button>
 
